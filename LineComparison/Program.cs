ae4be544fff413a6fed63bc069873bce2eac2040
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 15:57:22 +0530

    [Rakshit] Add: UC1 Calculate line length

---
+++ LineComparison/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------Welcome to Line Comparison Program---------\n");
+            Comparison comparison = new Comparison(2,4,6,9);
+            comparison.CalculateLength();
+            Console.ReadLine();
+
+        }
+    }
+}
